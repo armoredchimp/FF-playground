e1d954ce4d1265cedaa4bde9cb248eff9b58a551
Matt K <mattkasmir@gmail.com>
2025-01-07 21:42:03 -0800

    frustration

---
+++ src/lib/Team.svelte
@@ -0,0 +1,15 @@
+<script>
+
+    let {
+        team = {
+        name: '',
+        draftOrder: 0,
+        attackers: [],
+        midfielders: [],
+        defenders: [],
+        keepers: [],
+        playerCount: 0
+    }} = $props();
+
+  
+</script>
